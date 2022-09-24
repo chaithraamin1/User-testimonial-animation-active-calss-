180b61d818d3e834dc4acea955d0932ccb21b3b8
chai <chaithra.amin98@gmail.com>
2022-09-24 19:01:36 +0530

    user-testimonial-using-css-javscript-html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as $ from "jquery";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'animated-user-testimonial';
+  ngOnInit() {}
+activeSelectedText(){
+debugger
+  var usertexts: any = document.getElementsByClassName("usertext");
+  for (var text of usertexts) {
+    text.classList.remove("active-text")
+  }
+  var userimages: any = document.getElementsByClassName("user-img");
+  for (var img of userimages) {
+    img.classList.remove("active-pic")
+  }
+  var i=Array.from(userimages).indexOf(event?.target);
+  usertexts[i].classList.add("active-text");
+  userimages[i].classList.add("active-pic");
+}
+
 }
